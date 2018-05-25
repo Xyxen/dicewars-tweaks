c6f0ef4086eb5db5b6f1705e0dfd58130a31bb7f
Xyxen <XyxenGM@gmail.com>
2018-05-24 22:29:49 -0600

    Fix some rolls still showing in CPU-only fast play
    Fast play mode needs to account for the case where the player's control
    is replaced with AI for CPU-only mode.

--- inject/fast_ai_battles.js
+++ inject/fast_ai_battles.js
@@ -63,7 +63,7 @@ start_battle = function()
     // Check if the player is even involved in this fight.
     var from = game.adat[game.area_from].arm;
     var to = game.adat[game.area_to].arm;
-    if (from != game.user && to != game.user) {
+    if ((from != game.user && to != game.user) || cpu_play) {
         // Skip the dice animation if they're not.
         waitcount = 15;
         timer_func = after_battle;
